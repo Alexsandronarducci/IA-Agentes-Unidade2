4fde05f7b4a3f14612804f0da566b96d83042644
CauanVSouza <cauans.vs@gmail.com>
2026-04-06 18:54:35 -0300

    Update obstacle positions in Etapa2AgenteBaseadoEmModelo

--- Etapa2AgenteBaseadoEmModelo.java
+++ Etapa2AgenteBaseadoEmModelo.java
@@ -6,13 +6,24 @@ public class Etapa2AgenteBaseadoEmModelo {
         System.out.println("=== ETAPA 2: AGENTE REATIVO BASEADO EM MODELO ===");
 
         boolean[][] obstaculos = new boolean[GridUtils.N][GridUtils.N];
-        obstaculos[1][3] = true;
-        obstaculos[2][3] = true;
-        obstaculos[3][5] = true;
-        obstaculos[4][1] = true;
-        obstaculos[5][6] = true;
-        obstaculos[6][6] = true;
-        obstaculos[7][6] = true;
+        obstaculos[0][4] = true; 
+        obstaculos[1][0] = true; 
+        obstaculos[1][3] = true; 
+        obstaculos[2][3] = true; 
+        obstaculos[2][5] = true; 
+        obstaculos[3][2] = true; 
+        obstaculos[3][5] = true; 
+        obstaculos[4][4] = true; 
+        obstaculos[4][6] = true; 
+        obstaculos[4][7] = true; 
+        obstaculos[5][4] = true; 
+        obstaculos[5][6] = true; 
+        obstaculos[5][8] = true; 
+        obstaculos[6][4] = true; 
+        obstaculos[6][8] = true; 
+        obstaculos[7][4] = true; 
+        obstaculos[7][7] = true; 
+        obstaculos[7][8] = true;
 
         boolean[][] visitado = new boolean[GridUtils.N][GridUtils.N];
         Pos robo = GridUtils.posicaoAleatoriaLivre(obstaculos);
